9b01b8aced54c4a0c970ca28fb3d0c5945a91c6a
sfehlandt <sebafehlandt@gmail.com>
2017-11-11 19:42:30 -0300

    Create Resource model

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Resource(models.Model):
+
+    name = models.CharField(max_length=50, blank=True, verbose_name='nombre')
+    url = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        verbose_name = 'Recurso'
+        verbose_name_plural = 'Recursos'
+
+    def __str__(self):
+        return self.name
--- resources/tests/tests_models.py
+++ resources/tests/tests_models.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from ..models import Resource
 
 # Create your tests here.
+class ResourceModelTestCase(TestCase):
+    """This class defines the test suite for the  Resources model tests"""
+
+    # Arrange
+    def setUp(self):
+        self.old_count = Resource.objects.count()
+        self.resource = Resource(
+            name = 'recurso',
+            url = 'http://blablabla.com'
+        )
+
+    def test_model_create_resource(self):
+        """Test if you can create a resource directly in the model"""
+        # Act
+        self.resource.save()
+        # Assert
+        self.new_count = Resource.objects.count()
+        self.assertEquals(self.old_count+1, self.new_count)
+
+    def test_model_delete_resource(self):
+        """Test if you can delete a resource directly in the model"""
+        # Assert:
+        self.resource.save()
+        self.old_count = Resource.objects.count()
+        # Act
+        Resource.objects.filter(pk=self.resource.pk).delete()
+        # Assert
+        self.new_count = Resource.objects.count()
+        self.assertEquals(self.old_count-1, self.new_count)
