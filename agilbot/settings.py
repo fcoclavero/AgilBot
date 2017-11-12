0a584c9b6afa9b97d23b6b41fa5f1c6d68674fd4
Nicolas <nicolas_jara_rojas@hotmail.com>
2017-11-12 20:09:44 -0300

    Merge branch 'feature/refactor-models' into develop

--- agilbot/settings.py
+++ agilbot/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 # Heroku config
 
-#import dj_database_url
+import dj_database_url
 
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
