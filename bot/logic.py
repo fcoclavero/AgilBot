5f8ea48c49b37652b566b53776132d8b4ce172ac
Sebastian <sebafehlandt@gmail.com>
2017-11-17 19:12:12 -0300

    Clean code

--- bot/logic.py
+++ bot/logic.py
@@ -1,41 +1,4 @@
-from django.core.exceptions import ValidationError
-from django.core.validators import URLValidator
 from resources.models import Resource, Tag, Type
-
-
-def add_resource_link(msg_text):
-    split = msg_text.splt(" ")
-
-    tags = []
-    link = None
-    description = []
-
-    val = URLValidator(verify_exists=True)
-
-    # Retrieve fields
-    for txt in split:
-        if link is None:
-            try:
-                val(txt)
-                link = txt
-            except ValidationError:
-                pass
-
-        if txt[0] == "#":
-            tags.append(txt)
-        else:
-            description.append(txt)
-
-    if link is not None:
-        resource = Resource.objects.create(
-            name=description[0],
-            description=" ".join(description),
-            url=link
-        )
-        for t in tags:
-            tag = Tag.objects.create(name=t)
-            resource.tags.add(tag)
-        return True
 
 
 def add_resource_from_msg(msg, msg_type):
