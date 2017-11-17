41b696df1bf7c82c7183661184403482edb28cda
kloness <pablog.acc@gmail.com>
2017-11-17 11:46:18 -0300

    filter by week: url, view

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ from app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^semana/(?P<id>\d+)/$', views.week_view, name='week_view'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from resources import models
@@ -5,6 +6,21 @@ from resources import models
 
 def index(request):
     resources = models.Resource.objects.all().order_by('-create_timestamp')
+    weeks = models.Week.objects.all().order_by('-number')
+
+    context = {
+        'resources': resources,
+        'weeks': weeks
+    }
+    return render(request, 'index.html', context)
+
+
+def week_view(request, id):
+    if not models.Week.objects.filter(id=id):
+        raise Http404('No existe esa semana')
+
+    week = models.Week.objects.get(id=id)
+    resources = models.Resource.objects.filter(weeks=week).order_by('-create_timestamp')
     weeks = models.Week.objects.all().order_by('-number')
 
     context = {
