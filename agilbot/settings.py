be19856553a8cda2685afbf205bff33226c652e6
Francisco Clavero <fcoclavero32@gmail.com>
2017-11-23 12:17:05 -0300

    Updated settings.

--- agilbot/settings.py
+++ agilbot/settings.py
@@ -127,12 +127,12 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static_assets'),
 )
 
 # Simplified static file serving.
